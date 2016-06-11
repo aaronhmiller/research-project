8c3b070b2d0edeae404f7074c1561d49eaf34d98
Aaron Miller <aaronmiller@gmail.com>
2016-06-11 15:03:33 -0700

    init checkin

---
+++ quicklookup.js
@@ -0,0 +1,18 @@
+/***
+preamble: script to make researching company incrementally faster & easier
+plus an excuse to try making a barely useful Node.js tool ;)
+usage: node quicklookup.js -b <company_name>
+expected behavior: launch two default browser windows, one to crunchbase & another to glassdoor
+
+requires: node, openurl & minimist 
+***/
+
+var argv = require('minimist')(process.argv.slice(2));
+var url = argv.b;
+console.dir(url);
+require("openurl").open("http://crunchbase.com/organization/" + url);
+require("openurl").open("https://www.glassdoor.com/Reviews/index.htm");
+
+//just leaving here b/c handy to mail from a server-side script!
+//require("openurl").mailto(["aaronmiller@gmail.com"],
+//    { subject: "Info about " + url, body: "This is an automatically sent email!" });
