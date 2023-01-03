ceaf940be76da780651307744aaa57fb6b9cbb2d
Aaron Miller <aaronmiller@gmail.com>
2023-01-02 20:49:44 -0800

    updated logic and readme

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-## research-project
+# research-project
 My first try at [Node](nodejs.org). Be nice. It's a simple lookup script which launches a couple default browser instances locally based on the command-line -b &lt;input&gt; value. I did it to simplify a job search, but you can use it for any sort of lookups you might want to do.
 
-usage: `$> node lookup.js -b <company_name>`
-dependencies: minimizer, openurl (use npm install &lt;arg&gt;)
+## usage:
+`$> npm install minimist openurl`
+`$> node quicklookup.js -b <company_name>`
--- quicklookup.js
+++ quicklookup.js
@@ -11,8 +11,7 @@ var argv = require('minimist')(process.argv.slice(2));
 var url = argv.b;
 console.dir(url);
 require("openurl").open("http://crunchbase.com/organization/" + url);
-require("openurl").open("https://www.glassdoor.com/Reviews/index.htm");
-
+require("openurl").open("https://www.glassdoor.com/Search/results.htm?keyword=" + url);
 //just leaving here b/c handy to mail from a server-side script!
 //require("openurl").mailto(["aaronmiller@gmail.com"],
 //    { subject: "Info about " + url, body: "This is an automatically sent email!" });
